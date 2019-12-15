194a05501648032932dfaccc4a5cc57d51222c82
ivan-company <ivan.company@hootsuite.com>
2019-12-15 15:49:34 -0800

    Fixed export script

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
-const fetch = require('isomorphic-unfetch');
 const withCSS = require('@zeit/next-css')
+const fs = require('fs')
 
 module.exports = withCSS({
   exportTrailingSlash: true,
   exportPathMap: async function() {
     const paths = {
       '/': { page: '/' },
-      '/about': { page: '/about' }
     };
-    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-    const data = await res.json();
-    const shows = data.map(entry => entry.show);
 
-    shows.forEach(show => {
-      paths[`/show/${show.id}`] = { page: '/show/[id]', query: { id: show.id } };
+    const posts = fs.readdirSync('./posts')
+      .map(p => p.split('.')[0])
+
+    posts.forEach(post => {
+      paths[`/posts/${post}`] = { page: '/posts/[slug]', query: { slug: post } };
     });
 
     return paths;
